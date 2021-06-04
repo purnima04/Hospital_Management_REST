ac2d04b744b1dfbf3e09f8950571cef9ee462504
Purnima <purnima@admins-MacBook-Pro.local>
2021-06-04 11:17:43 +0530

    Service Commit - https://github.com/purnima04/Hospital_Management_REST/issues/4#issue-910323750

--- src/main/java/com/banfico/hospital_management/Services/HospitalService.java
+++ src/main/java/com/banfico/hospital_management/Services/HospitalService.java
@@ -1,2 +1,41 @@
-package com.banfico.hospital_management.Services;public class HospitalService {
+package com.banfico.hospital_management.Services;
+
+import com.banfico.hospital_management.Dao.HospitalRepository;
+import com.banfico.hospital_management.Entity.HospitalEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HospitalService implements HospitalServiceInterface{
+
+    @Autowired
+    private HospitalRepository hospitalRepository;
+
+    @Override
+    public List<HospitalEntity> getHospitals() {
+        return hospitalRepository.findAll();
+    }
+
+    @Override
+    public HospitalEntity getHospital(String hospital_Id) {
+        return hospitalRepository.getById(hospital_Id);
+    }
+
+    @Override
+    public HospitalEntity addHospital(HospitalEntity hospitalEntity) {
+        return hospitalRepository.save(hospitalEntity);
+    }
+
+    @Override
+    public HospitalEntity updateHospital(HospitalEntity hospitalEntity) {
+        return hospitalRepository.save(hospitalEntity);
+    }
+
+    @Override
+    public void deleteHospital(String hospital_Id) {
+        HospitalEntity deletedEntity=hospitalRepository.getOne(hospital_Id);
+        hospitalRepository.delete(deletedEntity);
+    }
 }
