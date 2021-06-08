89603c3759cb9c2071bf5fe5adf557a8c22446e1
purnima04 <purnima.maheshwari@banfico.com>
2021-06-08 18:49:38 +0530

    Updated Main file - https://github.com/purnima04/Hospital_Management_REST/issues/1#issue-909446010

--- src/main/java/com/banfico/hospital_management/HospitalManagementApplication.java
+++ src/main/java/com/banfico/hospital_management/HospitalManagementApplication.java
@@ -2,7 +2,9 @@ package com.banfico.hospital_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HospitalManagementApplication {
 
