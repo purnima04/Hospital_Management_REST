377eeb43f1fd4605a372acbc0e2d334b6ab89a0b
purnima04 <purnima.maheshwari@banfico.com>
2021-06-12 20:50:47 +0530

    Updation with exceptions - https://github.com/purnima04/Hospital_Management_REST/issues/4#issue-910323750

--- src/main/java/com/banfico/hospital_management/Services/HospitalService.java
+++ src/main/java/com/banfico/hospital_management/Services/HospitalService.java
@@ -4,16 +4,63 @@ import com.banfico.hospital_management.Dao.DoctorRepository;
 import com.banfico.hospital_management.Dao.HospitalRepository;
 import com.banfico.hospital_management.Entity.DoctorEntity;
 import com.banfico.hospital_management.Entity.HospitalEntity;
+import com.banfico.hospital_management.ExceptionHandler.NoDataFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.List;
-import java.util.ServiceLoader;
+import java.util.Optional;
+
+import com.banfico.hospital_management.ExceptionHandler.HospitalNotFoundException;
+
+import javax.validation.Valid;
 
 @Service
 public class HospitalService implements HospitalServiceInterface{
+    @Autowired
+    private HospitalRepository hospitalRepository;
+    @Autowired
+    private DoctorRepository doctorRepository;
 
+    @Override
+    public List<HospitalEntity> findAll() {
+        List<HospitalEntity> hospitals = (List<HospitalEntity>) hospitalRepository.findAll();
+        if (hospitals.isEmpty()) {
+            throw new NoDataFoundException();
+        }
+        return hospitals;
+    }
+
+    public HospitalEntity update(int hospital_Id, @Valid HospitalEntity hospitalEntity) {
+        Optional<HospitalEntity> p = hospitalRepository.findById(hospital_Id);
+        if (p.isPresent()) {
+            hospitalEntity.setHospital_Id(hospital_Id);
+            hospitalRepository.save(hospitalEntity);
+            return hospitalEntity;
+        } else {
+            throw new HospitalNotFoundException("Hospital does not exist with this id " + hospital_Id);
+        }
+    }
+
+    @Override
+    public HospitalEntity findById(int hospital_Id) {
+        return hospitalRepository.findById(hospital_Id).orElseThrow(()->new HospitalNotFoundException(hospital_Id));
+    }
+
+    @Override
+    public HospitalEntity save(HospitalEntity hospitalEntity) {
+        return hospitalRepository.save(hospitalEntity);
+    }
+    @Override
+    public String deleteHospital(int hospital_Id){
+        Optional hospitals = hospitalRepository.findById(hospital_Id);
+        if (hospitals.isEmpty()) {
+            throw new NoDataFoundException();
+        }else {
+            hospitalRepository.deleteById(hospital_Id);
+            return ("Hospital got deleted");
+        }
+    }
+/*
     @Autowired
     private HospitalRepository hospitalRepository;
     @Autowired
@@ -35,8 +82,9 @@ public class HospitalService implements HospitalServiceInterface{
     }
 
     @Override
-    public HospitalEntity updateHospital(HospitalEntity hospitalEntity) {
-        return hospitalRepository.save(hospitalEntity);
+    public void updateHospital(Long hospital_Id, HospitalEntity hospitalEntity) {
+        HospitalEntity toUpdate=hospitalRepository.findById(hospital_Id).get();
+        hospitalRepository.save(toUpdate);
     }
 
     @Override
@@ -47,4 +95,40 @@ public class HospitalService implements HospitalServiceInterface{
     public List<DoctorEntity> getDoctors() {
         return doctorRepository.findAll();
     }
+
+ */
+    /*
+    @Autowired
+    private HospitalRepository hospitalRepository;
+
+    @Override
+    public List<HospitalEntity> getHospitals(){
+        return hospitalRepository.findAll();
+    }
+
+    @Override
+    public HospitalEntity getHospital(int hospital_Id){
+        return hospitalRepository.findById(hospital_Id);
+    }
+
+    @Override
+    public HospitalEntity addHospital(HospitalEntity hospitalEntity) {
+        hospitalRepository.save(hospitalEntity);
+        return hospitalEntity;
+    }
+
+    @Override
+    public HospitalEntity updateHospital(HospitalEntity hospitalEntity) {
+        hospitalRepository.save(hospitalEntity);
+        return hospitalEntity;
+    }
+
+    @Override
+    public HospitalEntity deleteHospital(int hospital_Id){
+        HospitalEntity deletedHospital = hospitalRepository.getById(hospital_Id);
+        hospitalRepository.delete(deletedHospital);
+        return deletedHospital;
+    }
+
+     */
 }
--- src/main/java/com/banfico/hospital_management/Services/HospitalServiceInterface.java
+++ src/main/java/com/banfico/hospital_management/Services/HospitalServiceInterface.java
@@ -3,13 +3,21 @@ package com.banfico.hospital_management.Services;
 import com.banfico.hospital_management.Entity.DoctorEntity;
 import com.banfico.hospital_management.Entity.HospitalEntity;
 
+import javax.print.Doc;
 import java.util.List;
+import java.util.Optional;
 
 public interface HospitalServiceInterface{
-    public List<HospitalEntity> getHospitals();
-    public HospitalEntity getHospital(Long hospital_Id);
-    public HospitalEntity addHospital(HospitalEntity hospitalEntity);
-    public HospitalEntity updateHospital(HospitalEntity hospitalEntity);
-    public void deleteHospital(Long hospital_Id);
-    //public List<DoctorEntity> getDoctors();
+    /*public List<HospitalEntity> getHospitals() throws Exception;
+    public HospitalEntity getHospital(int hospital_Id)throws Exception;
+    public HospitalEntity addHospital(HospitalEntity hospitalEntity) throws Exception;
+    public HospitalEntity updateHospital(HospitalEntity hospitalEntity) throws Exception;
+    public HospitalEntity deleteHospital(int hospital_Id) throws Exception;
+     */
+
+    public HospitalEntity findById(int hospital_Id);
+    public HospitalEntity save(HospitalEntity hospitalEntity);
+    public List<HospitalEntity> findAll();
+    public HospitalEntity update(int hospital_Id, HospitalEntity hospitalEntity);
+    public String deleteHospital(int hospital_Id);
 }
