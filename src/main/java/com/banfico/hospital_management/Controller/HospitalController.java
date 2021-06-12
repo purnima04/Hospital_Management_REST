77d3376234473d26eaf430692ff4451127bb2a7d
purnima04 <purnima.maheshwari@banfico.com>
2021-06-12 20:43:34 +0530

    Updated controller with few Exceptions - https://github.com/purnima04/Hospital_Management_REST/issues/3#issue-910320395

--- src/main/java/com/banfico/hospital_management/Controller/HospitalController.java
+++ src/main/java/com/banfico/hospital_management/Controller/HospitalController.java
@@ -1,19 +1,24 @@
 package com.banfico.hospital_management.Controller;
 
+import com.banfico.hospital_management.API_Paths.API_paths;
 import com.banfico.hospital_management.Entity.DoctorEntity;
 import com.banfico.hospital_management.Entity.HospitalEntity;
-import com.banfico.hospital_management.Services.DoctorService;
 import com.banfico.hospital_management.Services.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping
+@CrossOrigin(origins = "*")
+@RequestMapping(API_paths.hospitalPathCtrl.CTRL)
 public class HospitalController {
-
+/*
     @Autowired
     private HospitalService hospitalService;
 
@@ -28,10 +33,10 @@ public class HospitalController {
     }
 
     @RequestMapping(value = "/details/{hospital_Id}", method = RequestMethod.GET)
-    public ResponseEntity<HospitalEntity> getHospital(@Valid @PathVariable Long hospital_Id){
+    public ResponseEntity<HospitalEntity> getHospital(@Valid @PathVariable Long hospital_Id) {
         try {
             return new ResponseEntity<>(hospitalService.getHospital(hospital_Id), HttpStatus.FOUND);
-        }catch (Exception exception){
+        } catch (Exception exception) {
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -45,11 +50,12 @@ public class HospitalController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
-    @RequestMapping(value = "/details", method = RequestMethod.PUT)
-    public ResponseEntity<HospitalEntity> updateHospital(@Valid @RequestBody HospitalEntity hospitalEntity){
+    @RequestMapping(value = "/details/{hospital_Id}", method = RequestMethod.PUT)
+    public ResponseEntity<HospitalEntity> updateHospital(@PathVariable("hospital_Id") Long hospital_Id,
+                                                        @Valid @RequestBody HospitalEntity hospitalEntity){
         try {
-            this.updateHospital(hospitalEntity);
-            return new ResponseEntity<>(hospitalService.updateHospital(hospitalEntity), HttpStatus.OK);
+            hospitalService.updateHospital(hospital_Id, hospitalEntity);
+            return new ResponseEntity<>(hospitalService.getHospital(hospital_Id), HttpStatus.OK);
         }catch (Exception exception){
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
@@ -67,5 +73,88 @@ public class HospitalController {
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+ */
+    /*
+    @Autowired
+    private HospitalService hospitalService;
+
+    @GetMapping("/details")
+    public ResponseEntity<Object> getHospitals(){
+        try {
+            List<HospitalEntity> hospitalEntity = hospitalService.getHospitals();
+            return new ResponseEntity<>(hospitalEntity, HttpStatus.FOUND);
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+    @GetMapping("/details/{hospital_Id}")
+    public ResponseEntity<HospitalEntity> getHospital(@Valid @PathVariable int hospital_Id){
+        HospitalEntity hospitalEntity;
+        try {
+            hospitalEntity = hospitalService.getHospital(hospital_Id);
+            return new ResponseEntity<>(hospitalEntity,HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+    @PostMapping("/details")
+    public ResponseEntity<HospitalEntity> addHospital(@Valid @RequestBody HospitalEntity hospitalEntity) {
+        HospitalEntity hospital=hospitalService.addHospital(hospitalEntity);
+        return new ResponseEntity<>(hospital,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/details")
+    public ResponseEntity<HospitalEntity> updateHospital(@Valid @RequestBody HospitalEntity hospitalEntity){
+        HospitalEntity hospitalFound = hospitalService.updateHospital(hospitalEntity);
+        return new ResponseEntity<>(hospitalFound,HttpStatus.OK);
+    }
+    @DeleteMapping("/details")
+    public ResponseEntity<HospitalEntity> deleteHospital(@Valid @RequestParam("hospital_Id") int hospital_Id){
+        HospitalEntity deletedHospital;
+        try {
+            deletedHospital = hospitalService.deleteHospital(hospital_Id);
+            return new ResponseEntity<>(deletedHospital,HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+     */
+    @Autowired
+    private HospitalService hospitalService;
+
+    @GetMapping(value = "/details")
+    public List<HospitalEntity> getHospitals() {
+        return hospitalService.findAll();
+    }
+
+    @GetMapping(value = "/details/{hospital_Id}")
+    public HospitalEntity getHospital(@PathVariable int hospital_Id) {
+        return hospitalService.findById(hospital_Id);
+    }
+
+    @PutMapping(value = "/details/{hospital_Id}",
+                consumes = MediaType.APPLICATION_JSON_VALUE,
+                produces = MediaType.APPLICATION_JSON_VALUE)
+    public HospitalEntity updateHospital(@PathVariable(name = "hospital_Id", required = true) int hospital_Id, @Valid @RequestBody HospitalEntity hospitalEntity) {
+        //HospitalEntity hospitalFound = hospitalService.update(hospitalEntity);
+        return hospitalService.update(hospital_Id, hospitalEntity);
+    }
+
+    @PostMapping(value = "/details", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public HospitalEntity addHospital(@RequestBody @Valid HospitalEntity hospitalEntity) {
+        return hospitalService.save(hospitalEntity);
+    }
+
+    @RequestMapping(value = "/details/{hospital_Id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteHospital(@PathVariable int hospital_Id){
+        hospitalService.deleteHospital(hospital_Id);
+        return ResponseEntity.accepted().body("Successful deletion");
     }
 }
