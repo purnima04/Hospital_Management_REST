2485c8dcfd2fc5588d5ff440fea592edf8fafdc5
purnima04 <purnima.maheshwari@banfico.com>
2021-06-12 20:40:28 +0530

    Added extra file incase of adding multiple controllers - https://github.com/purnima04/Hospital_Management_REST/issues/3#issue-910320395

---
+++ src/main/java/com/banfico/hospital_management/API_Paths/API_paths.java
@@ -0,0 +1,25 @@
+package com.banfico.hospital_management.API_Paths;
+
+public class API_paths {
+    private static final String BASE_PATH = "/api";
+    private static final String HOSPITAL_PATH = "/hospital";
+    //private static final String PATIENT_PATH = "/patient";
+    //private static final String DOCTOR_PATH = "/doctors";
+    //private static final String BILL_PATH = "/doctors";
+
+    public static final class hospitalPathCtrl {
+        public static final String CTRL = BASE_PATH + HOSPITAL_PATH;
+    }
+/*
+    public static final class patientPathCtrl {
+        public static final String CTRL = BASE_PATH + PATIENT_PATH;
+    }
+
+    public static final class DoctorPathCtrl {
+        public static final String CTRL = BASE_PATH + DOCTOR_PATH;
+    }
+    public static final class billPathCtrl {
+        public static final String CTRL = BASE_PATH + BILL_PATH;
+    }
+ */
+}
