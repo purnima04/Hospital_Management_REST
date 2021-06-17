fbf7a65dbe8b7ee5b7df73ff78d782ae2f4ea01f
purnima04 <purnima.maheshwari@banfico.com>
2021-06-17 12:28:45 +0530

    API_PATHS Updated - https://github.com/purnima04/Hospital_Management_REST/issues/3#issue-910320395

--- src/main/java/com/banfico/hospital_management/API_Paths/API_paths.java
+++ src/main/java/com/banfico/hospital_management/API_Paths/API_paths.java
@@ -3,15 +3,14 @@ package com.banfico.hospital_management.API_Paths;
 public class API_paths {
     private static final String BASE_PATH = "/api";
     private static final String HOSPITAL_PATH = "/hospital";
-    //private static final String PATIENT_PATH = "/patient";
-    //private static final String DOCTOR_PATH = "/doctors";
-    //private static final String BILL_PATH = "/doctors";
+    private static final String PATIENT_PATH = "/patient";
+    private static final String DOCTOR_PATH = "/doctors";
 
     public static final class hospitalPathCtrl {
         public static final String CTRL = BASE_PATH + HOSPITAL_PATH;
     }
 
-    /*
+
     public static final class patientPathCtrl {
         public static final String CTRL = BASE_PATH + PATIENT_PATH;
     }
@@ -19,9 +18,4 @@ public class API_paths {
     public static final class DoctorPathCtrl {
         public static final String CTRL = BASE_PATH + DOCTOR_PATH;
     }
-
-    public static final class billPathCtrl {
-        public static final String CTRL = BASE_PATH + BILL_PATH;
-    }
- */
 }
